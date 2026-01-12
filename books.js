3943872c3b3ca5cb364d815fb471c5f9154d1c25
AmeliaJefferson2020 <ameliathompson.ace@gmail.com>
2026-01-12 03:23:44 -0500

    Finished App

--- books.js
+++ books.js
@@ -5,9 +5,9 @@ function renderBooks(filter) {
 
   console.log(filter);
   if (filter === "LOW_TO_HIGH") {
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
+    books.sort((a, b) => (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice));
   } else if (filter === "HIGH_TO_LOW") {
-    books.sort((a, b) => b.originalPrice - a.originalPrice);
+    books.sort((a, b) => (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice));
   } else if (filter === "RATING") {
     books.sort((a, b) => b.rating - a.rating);
   }
@@ -25,7 +25,6 @@ ${ratingsHTML(book.rating)}
 </div>
 <div class="book__price">
  ${priceHTML(book.originalPrice, book.salePrice)}
-<span> $${book.originalPrice.toFixed(2)}</span>
 </div>
 </div>`;
     })
@@ -38,9 +37,7 @@ function priceHTML(originalPrice, salePrice) {
   if (!salePrice) {
     return `$${originalPrice.toFixed(2)}`
   }
-  else {
-    return `<span class="book__price--normal">$${originalPrice}</span>$${salePrice}`
-  }
+    return `<span class="book__price--normal">$${originalPrice.toFixed(2)}</span>$${salePrice.toFixed(2)}`
 }
 
 function ratingsHTML(rating) {
